d59be731db7fd719b276e76e8a067a88311524ae
langay <brianlangay4@gmail.com>
2022-08-01 12:28:53 +0800

    sent

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,36 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        /*program starts here
+        first  created name fetching input
+        where a user can input the name
+        and this name will be carried for some outputs later
+         */
         System.out.println("Welcome input your name");
         Scanner input1 = new Scanner(System.in);
         String name = input1.nextLine();
         {
+            //first name output
             System.out.println("welcome " + name);
         }
+        //game start here
         System.out.println("Guessing number game");
+        //giving  user option to either lay or not
         System.out.println("if you wish to play press1");
+        //ii take the user option by scanner and work with it
         Scanner input2 = new Scanner(System.in);
         String c = input2.nextLine();
+        /*first if statement used to direct user into a game or outside the game
+        so first i used if statement then try catch block to handle typo
+        then i continued by using other different statements to perform game options and redirection
+         */
         if (c.equals("1")) {
             System.out.println("Game starts ");
-            // Loading
+            /* Loading
+            here wth the use of try cath block exception and i created a loading pause
+            user will hav eto wai for about 3000 milliseconds
+            this is done by thread try catch control
+             */
             try {
                 System.out.println("loading.......");
                 System.out.println("do not press anything please wait");
@@ -23,22 +40,30 @@ public class Main {
             } catch (InterruptedException e) {
                 System.out.println("Thread is interrupted Game error");
             }
+            //welcoming a user to Start the game
             System.out.println("......................................");
             System.out.println("Guess 1 digit number press 1 if your ready");
         }
+        //here we handle wrong input from a previous given option
         else {
             System.out.println("wrong input ");
             System.out.println("Exiting Game");
             System.out.println("--------------------------------------------");
         }
         {
+            /*here i start to work with the game engine
+            from now the input 3 scanner object will be working for first game
+            inside here i created a little Algorithm by using aRandom number method
+             */
             Scanner input3 = new Scanner(System.in);
             int d = input3.nextInt();
             if (d == (1)) {
+                //i created three trial game input you can only guess three times
                 System.out.println("........................");
                 System.out.println("you have 3 trials to start guessing");
                 System.out.println("**********************");
                 System.out.println("guess the first number");
+
                 Scanner input4 = new Scanner(System.in);
                 int e = input4.nextInt();
                 Random number1 = new Random();
